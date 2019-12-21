ef68ee06335adce80e998e1c0ce66724c2b7e730
adaploska <adawagrocka@wp.pl>
2019-12-21 18:10:03 +0100

    testing increment & decrement button

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      counter: 0,
+      error: false
+    };
+  }
+  decrement = () => {
+    if (this.state.counter === 0) {
+      this.setState({
+        counter: 0,
+        error: true
+      });
+    } else {
+      this.setState({
+        counter: this.state.counter - 1,
+        error: false
+      });
+    }
+  };
+  increment = () => {
+    if (this.state.error) {
+      this.setState({ error: false });
+    }
+    this.setState({ counter: this.state.counter + 1 });
+  };
+
+  render() {
+    const errorClass = this.state.error ? "" : "hidden";
+
+    return (
+      <div data-test="component-app">
+        <h1 data-test="counter-display">
+          The counter is currently {this.state.counter}
+        </h1>
+        {/* {this.state.error === true && ( */}
+        <p
+          data-test="display-error-massage"
+          style={{ color: "red" }}
+          className={`error ${errorClass}`}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          The counter cannot go below 0
+        </p>
+        {/* )} */}
+        <button data-test="increment-button" onClick={this.increment}>
+          Increment counter
+        </button>
+        <button data-test="decrement-button" onClick={this.decrement}>
+          Decrement counter
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
